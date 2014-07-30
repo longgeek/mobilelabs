15bb2eb29b8a385a86f528b49058e2a9ef6c358c
Longgeek <longgeek@thstack.com>
2014-07-30 18:39:12 +0800

    Add dhcp iptables ports

--- puppet/modules/neutron-network/manifests/iptables.pp
+++ puppet/modules/neutron-network/manifests/iptables.pp
@@ -5,4 +5,20 @@ class neutron-network::iptables {
         path    => $command_path,
         unless  => "grep '9697 -j ACCEPT' /etc/sysconfig/iptables",
     }
+
+    exec { '67':
+        command => 'iptables -A INPUT -p udp -m multiport --dports 67 -j ACCEPT; \
+                    /etc/init.d/iptables save',
+        path    => $command_path,
+        unless  => "grep '--dports 67 -j ACCEPT' /etc/sysconfig/iptables",
+        require => Exec['9697'],
+    }
+
+    exec { '68':
+        command => 'iptables -A OUTPUT -p udp -m multiport --dports 68 -j ACCEPT; \
+                    /etc/init.d/iptables save',
+        path    => $command_path,
+        unless  => "grep '--dports 68 -j ACCEPT' /etc/sysconfig/iptables",
+        require => Exec['67'],
+    }
 }
