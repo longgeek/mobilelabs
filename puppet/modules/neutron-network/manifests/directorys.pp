fca9a90ff5bc7ee89a7a2befbde75bd0d66006de
Longgeek <longgeek@thstack.com>
2014-08-02 19:53:08 +0800

    Add haproxy requirement dirs.

--- puppet/modules/neutron-network/manifests/directorys.pp
+++ puppet/modules/neutron-network/manifests/directorys.pp
@@ -4,7 +4,7 @@ class neutron-network::directorys {
         mode   => 0755,
     }
 
-    file { ['/etc/neutron', '/var/log/neutron', '/etc/neutron/plugins', '/etc/neutron/plugins/ml2', '/etc/neutron/rootwrap.d', '/var/run/neutron']:
+    file { ['/etc/neutron', '/var/log/neutron', '/etc/neutron/plugins', '/etc/neutron/plugins/ml2', '/etc/neutron/rootwrap.d', '/var/run/neutron', '/etc/neutron/services/', '/etc/neutron/services/loadbalancer', '/etc/neutron/services/loadbalancer/haproxy']:
         ensure  => directory,
         require => File['/var/lib/neutron'],
     }
